55325c7365bd1e2e4319a2830da85030e0965854
Chanon Jenakom <chanonjenakom@gmail.com>
2018-05-18 16:11:28 +0700

    Disable warnings

--- mnist.py
+++ mnist.py
@@ -1,4 +1,8 @@
+import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
+
+# Disable warnings
+tf.logging.set_verbosity(tf.logging.ERROR)
 
 def read_dataset():
     return input_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
