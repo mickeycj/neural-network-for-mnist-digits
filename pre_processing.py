daed71f6c189f90b9a3ea27d25b0108f5f25a326
yeast72 <yeast72@gmail.com>
2018-05-21 02:38:35 +0700

    write image sorted

--- pre_processing.py
+++ pre_processing.py
@@ -3,49 +3,50 @@ import numpy as np
 
 img_path = "/home/wisarut/Desktop/picture1.png"
 
-im = cv2.imread(img_path)
+#read image
+img = cv2.imread(img_path)
 
-im_gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-im_gray = cv2.GaussianBlur(im_gray,(5,5),0)
+# read the image in grayscale 
+img_gray = cv2.imread(img_path,0)
+cv2.imshow('load image grayscale',img_gray)
+cv2.waitKey()
 
-ret, im_th = cv2.threshold(im_gray,90,255,cv2.THRESH_BINARY_INV)
+# Apply Gaussain filtering
+img_gray = cv2.GaussianBlur(img_gray,(5,5), 0)
 
-_,ctrs, hier = cv2.findContours(im_th.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# binary in gray
+ret, thresh = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV) 
+cv2.imshow('thresh', thresh)
+cv2.waitKey(0)
 
-rects = [cv2.boundingRect(ctr) for ctr in ctrs]
-i = 10
-for rect in rects:
-  # #Draw the rectangles
-  # cv2.rectangle(im, (rect[0],rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
+# dilation
+kernel = np.ones((3, 3), np.uint8)  # values set for this image only - need to change for different images
+img_dilation = cv2.dilate(thresh, kernel, iterations=1)
+cv2.imshow('dilated', img_dilation)
+cv2.waitKey(0)
 
-  # #Make the rectangles
-  # leng = int(rect[3] * 1.6)
-  # pt1 = int(rect[1] + rect[3] // 2- leng // 2)
-  # pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-  # roi = im_th[pt1:pt1 + leng, pt2:pt2+leng]
+# find contours
+_,ctrs, hier = cv2.findContours(img_dilation.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-  # roi = cv2.resize(roi, (28,28), interpolation=cv2.INTER_AREA)
-  # roi = cv2.dilate(roi, (3,3))
+# sort contours from x + y
+sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0] + cv2.boundingRect(ctr)[1])
 
-  i = i -1
-  x, y, w, h = cv2.boundingRect(ctr)
-  # Getting ROI
-  leng = int(rect[3] * 1.6)
-  pt1 = int(rect[1] + rect[3] // 2- leng // 2)
-  pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-  roi = im_th[pt1:pt1 + leng, pt2:pt2+leng]
+for i,rect in enumerate(sorted_ctrs):
+  # Get bounding box
+  x, y, w, h = cv2.boundingRect(rect)
+
+  # Getting ROI and resize image
+  leng = int(h * 1.6)
+  pt1 = int(y + h // 2- leng // 2)
+  pt2 = int(x + w // 2 - leng // 2)
+  roi = img_dilation[pt1:pt1 + leng, pt2:pt2+leng]
   roi = cv2.resize(roi, (28,28), interpolation=cv2.INTER_AREA)
   roi = cv2.dilate(roi, (3,3))
 
   # show ROI
   cv2.imshow( str(pt1) +',' + str(pt2) +' : '+str(i),roi)
-  cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), 1)
   cv2.waitKey(0)
   
-  # save only the ROI's which contain a valid information
-  if h > 10 and w > 10:
-    cv2.imwrite('roi\\{}.png'.format(i), roi) 
 
+  cv2.imwrite('roi\\{}.png'.format(i), roi) 
 
-cv2.imshow("ReSulting image woth rectangular rois", im)
-cv2.waitKey()
